e7f9487898c286f6f4aa20fdfdd4aee82f035848
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-04-08 14:01:42 +0700

    Fix button disabled prop

--- components/button.tsx
+++ components/button.tsx
@@ -5,15 +5,17 @@ type ButtonProps = {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-const Button = ({ children, className, ...props }: ButtonProps) => {
+const Button = ({ children, className, disabled, ...props }: ButtonProps) => {
   const { theme } = useTheme();
 
   if (theme === "dark") {
     return (
       <button
         className={cn("max-md:text-xs text-sm border-2 border-primary font-medium rounded-full px-2 md:px-3 py-1 hover:bg-primary hover:text-container disabled:bg-[#B8BABE] disabled:text-[#909295] disabled:border-0", className)}
+        disabled={disabled}
         {...props}
       >
         {children}
@@ -24,6 +26,7 @@ const Button = ({ children, className, ...props }: ButtonProps) => {
   return (
     <button
       className={cn("max-md:text-xs text-sm text-primary border-2 border-primary font-medium rounded-full px-2 md:px-3 py-1 hover:bg-primary hover:text-container disabled:bg-[#B8BABE] disabled:text-[#909295] disabled:border-0", className)}
+      disabled={disabled}
       {...props}
     >
       {children}

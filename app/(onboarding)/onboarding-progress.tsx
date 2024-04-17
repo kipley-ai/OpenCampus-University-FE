fa19be8d3d5e2d74685072336de914e94b50cdbd
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-04-17 20:10:55 +0700

    Improve onboarding progress

--- app/(onboarding)/onboarding-progress.tsx
+++ app/(onboarding)/onboarding-progress.tsx
@@ -1,101 +1,64 @@
-import Link from "next/link";
-
 interface BoxWithNumberProps {
-  number: number | string;
+  number: number;
+  isActive: boolean;
 }
 
-const BoxWithNumber: React.FC<BoxWithNumberProps> = ({ number }) => {
+const BoxWithNumber: React.FC<BoxWithNumberProps> = ({ number, isActive }) => {
   return (
-    <div className="flex h-5 w-5 items-center justify-center border border-primary bg-container">
-      <span className="text-sm font-bold">{number}</span>
+    <div
+      className={`flex h-5 w-5 items-center justify-center border border-2 bg-container ${isActive ? "border-primary text-primary" : "border-[#50575F]"}`}
+    >
+      <span className="text-sm">{number}</span>
     </div>
+  );
+};
+
+const ProgressItem = ({
+  number,
+  isActive,
+  children,
+}: {
+  number: number;
+  isActive: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    <li className="flex-1">
+      <div className="flex justify-center">
+        <span className="">
+          <BoxWithNumber number={number} isActive={isActive} />
+        </span>
+      </div>
+      <div className="flex justify-center">
+        <span
+          className={`text-center text-sm ${isActive ? "text-primary" : ""}`}
+        >
+          {children}
+        </span>
+      </div>
+    </li>
   );
 };
 
 export default function OnboardingProgress({ step = 1 }: { step?: number }) {
   return (
-    <div className="pb-8">
-      <div className="flex w-full justify-center">
-        <div className="relative">
-          <div
-            className="absolute inset-x-14 top-1/4 -mt-px h-0.5 bg-[#50575F] z-0"
-            aria-hidden="true"
-          ></div>
-          <ul className="relative flex justify-between gap-24">
-            <li className="flex-1">
-              <div className="flex justify-center">
-                <span className="">
-                  {step > 1 ? (
-                    <BoxWithNumber number="✓" />
-                  ) : (
-                    <BoxWithNumber number={1} />
-                  )}
-                </span>
-              </div>
-              <div className="flex justify-center">
-                <span
-                  className={`text-center text-sm font-medium`}
-                >
-                  Select Data Elements
-                </span>
-              </div>
-            </li>
-            <li className="flex-1">
-              <div className="flex justify-center">
-                <span className="">
-                  {step > 2 ? (
-                    <BoxWithNumber number="✓" />
-                  ) : (
-                    <BoxWithNumber number={2} />
-                  )}
-                </span>
-              </div>
-              <div className="flex justify-center">
-                <span
-                  className={`text-center text-sm font-medium`}
-                >
-                  Mint SFT
-                </span>
-              </div>
-            </li>
-            <li className="flex-1">
-              <div className="flex justify-center">
-                <span className="">
-                  {step > 3 ? (
-                    <BoxWithNumber number="✓" />
-                  ) : (
-                    <BoxWithNumber number={3} />
-                  )}
-                </span>
-              </div>
-              <div className="flex justify-center">
-                <span
-                  className={`text-center text-sm font-medium`}
-                >
-                  Create Chatbot
-                </span>
-              </div>
-            </li>
-            {/* <li className="flex-1">
-              <div className="flex justify-center">
-                <span className="">
-                  {step > 4 ? (
-                    <BoxWithNumber number="✓" />
-                  ) : (
-                    <BoxWithNumber number={4} />
-                  )}
-                </span>
-              </div>
-              <div className="flex justify-center">
-                <span
-                  className={`text-center text-sm font-medium`}
-                >
-                  Get KFI Token
-                </span>
-              </div>
-            </li> */}
-          </ul>
-        </div>
+    <div className="mb-2 flex w-full justify-center">
+      <div className="relative font-poppins font-semibold text-[#50575F]">
+        <div
+          className="absolute inset-x-8 xs:inset-x-14 top-[10px] z-0 -mt-px h-[2px] bg-[#50575F]"
+          aria-hidden="true"
+        ></div>
+        <ul className="relative z-20 flex justify-between gap-24">
+          <ProgressItem number={1} isActive={step === 1}>
+            Select Data Elements
+          </ProgressItem>
+          <ProgressItem number={2} isActive={step === 2}>
+            Mint SFT
+          </ProgressItem>
+          <ProgressItem number={3} isActive={step === 3}>
+            Create Chatbot
+          </ProgressItem>
+        </ul>
       </div>
     </div>
   );

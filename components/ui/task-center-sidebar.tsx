467d75efb5401de6a56070c157fbf1513248330d
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-04-24 20:33:21 +0700

    Add border effect to sidebar task center

--- components/ui/task-center-sidebar.tsx
+++ components/ui/task-center-sidebar.tsx
@@ -1,26 +1,23 @@
+import "./task-center-sidebar.css";
 import Image from "next/image";
-import TaskCenterImage from "public/images/join-box-nogrid.svg";
-import TaskCenterLightImage from "public/images/join-box-nogrid-light.svg";
+import TaskCenterImage from "public/images/task-center-sidebar-dark.svg";
+import TaskCenterLightImage from "public/images/task-center-sidebar-light.svg";
 import Link from "next/link";
 import Button from "@/components/button";
-import { useTheme } from 'next-themes';
+import { useTheme } from "next-themes";
 
 export default function TaskCenterSideBar() {
   const { theme } = useTheme();
 
   return (
-    <div className="relative flex flex-col items-center">
+    <div className="rainbow relative mx-5 flex flex-col items-center gap-4 px-1 py-8">
       {theme === "dark" ? (
         <Image src={TaskCenterImage} alt="Task Center" />
       ) : (
         <Image src={TaskCenterLightImage} alt="Task Center" />
       )}
-      <Link
-        href={"/task-center"}
-      >
-        <Button className="absolute bottom-12 inset-x-0 mx-auto w-3/4">
-          Task Center
-        </Button>
+      <Link href={"/task-center"} className="w-9/12">
+        <Button className="w-full">Task Center</Button>
       </Link>
     </div>
   );

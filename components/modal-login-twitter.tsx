f3ce97a37927a363c8dca86cac95363f1e045704
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-05-07 14:59:51 +0700

    Change Twitter modal close button

--- components/modal-login-twitter.tsx
+++ components/modal-login-twitter.tsx
@@ -4,12 +4,7 @@ import ModalBlank from "@/components/modal-blank-3";
 import Button from "@/components/button";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import CrossIcon from "public/images/cross-icon-2.png";
-import { usePathname } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-// import fbLogo from '@/public/images/icon-facebook.svg'
 import twtLogo from "@/public/images/logo-twitter.svg";
-// import emailLogo from '@/public/images/icon-linkedin.svg'
 
 export default function ModalLoginTwitter({
   isOpen,
@@ -35,15 +30,39 @@ export default function ModalLoginTwitter({
           <div className="w-80 text-2xl font-bold leading-10 text-heading">
             Sign in to continue
           </div>
-          <Image
-            className="h-[12px] w-[12px] cursor-pointer"
-            src={CrossIcon}
-            alt="cross icon"
+          <button
+            className="hover:text-secondary"
             onClick={(e) => {
               e.stopPropagation();
               setIsOpen(false);
             }}
-          />
+          >
+            <div className="sr-only">Close</div>
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-none hover:fill-secondary"
+            >
+              <rect
+                x="1"
+                y="1"
+                width="38"
+                height="38"
+                rx="19"
+                stroke="#353945"
+                strokeWidth="2"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M13.2929 13.2929C13.6834 12.9024 14.3166 12.9024 14.7071 13.2929L20 18.5858L25.2929 13.2929C25.6834 12.9024 26.3166 12.9024 26.7071 13.2929C27.0976 13.6834 27.0976 14.3166 26.7071 14.7071L21.4142 20L26.7071 25.2929C27.0976 25.6834 27.0976 26.3166 26.7071 26.7071C26.3166 27.0976 25.6834 27.0976 25.2929 26.7071L20 21.4142L14.7071 26.7071C14.3166 27.0976 13.6834 27.0976 13.2929 26.7071C12.9024 26.3166 12.9024 25.6834 13.2929 25.2929L18.5858 20L13.2929 14.7071C12.9024 14.3166 12.9024 13.6834 13.2929 13.2929Z"
+                fill="var(--color-heading)"
+              />
+            </svg>
+          </button>
         </div>
         <div className="flex max-w-full flex-col justify-center gap-5 py-5 md:w-96">
           <div className="flex flex-grow items-center justify-center rounded-3xl">

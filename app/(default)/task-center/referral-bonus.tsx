a3c87a3eb5876e34c7df1b7d34381c14cbd20ec5
Akira Rafhael Janson Pattirane <akira.rafhael@ti.ukdw.ac.id>
2024-05-13 17:02:48 +0800

    Update task center

--- app/(default)/task-center/referral-bonus.tsx
+++ app/(default)/task-center/referral-bonus.tsx
@@ -9,7 +9,7 @@ export const ReferralBonus = () => {
         <h1 className="text-lg font-semibold">Your Referral Link</h1>
         <div className="flex flex-row justify-between">
             <p className="w-1/3">Points will be added when your friend successfully signs up with your link.</p>
-            <div className="w-6/12 flex flex-row justify-between border items-center px-5 rounded-xl">
+            <div className="w-7/12 flex flex-row justify-between border items-center px-5 rounded-xl">
               <span className="opacity-50">https://oc-campus.com/reffer/?refid=325re33272ku</span>
               <span 
                 className="text-primary cursor-pointer" 

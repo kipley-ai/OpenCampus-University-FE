2bc1a621183ad8086b598f646fdab4f745c0151c
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-07-17 14:49:03 +0700

    Change app names in create app choose app page

--- app/(default)/nft/[id]/create-app/choose-app.tsx
+++ app/(default)/nft/[id]/create-app/choose-app.tsx
@@ -12,12 +12,12 @@ import { useGetPlugin } from "@/hooks/api/quiz_app";
 
 const apps = [
   {
-    name: "Chatbot",
+    name: "Study Companion",
     step: CHATBOT_APP,
     icon: CreateChatbotImg,
   },
   {
-    name: "Quiz",
+    name: "Quiz Generator",
     step: QUIZ_APP,
     icon: CreateQuizImg,
   },
@@ -32,7 +32,7 @@ export default function ChooseApp() {
   const [chosenApp, setChosenApp] = useState(CHATBOT_APP);
 
   const router = useRouter();
-  
+
   const { setStep, setPlugin } = useCreateAppContext();
   const { data: pluginList, isLoading } = useGetPlugin();
   if (isLoading) return null;
@@ -57,11 +57,11 @@ export default function ChooseApp() {
     <>
       <div className="mb-16 flex flex-col gap-8 md:mb-24">
         <h1 className="text-lg font-semibold text-primary">Select App</h1>
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-6">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-5">
           {apps.map((app) => (
             <button
               key={app.name}
-              className={`flex h-36 flex-col items-center justify-center gap-6 rounded-2xl border-2 bg-container px-4 py-6 text-xl font-medium ${app.step === "" ? "disabled text-secondary-text" : "text-primary"} enabled:hover:bg-secondary ${chosenApp === app.step ? "border-primary" : "border-border"}`}
+              className={`flex h-36 flex-col items-center justify-center gap-2 rounded-2xl border-2 bg-container px-8 py-6 text-lg font-medium xs:text-xl ${app.step === "" ? "disabled text-secondary-text" : "text-primary"} enabled:hover:bg-secondary ${chosenApp === app.step ? "border-primary" : "border-border"}`}
               onClick={() => handleChosenApp(app.step)}
             >
               {app.step !== "" && <Image src={app.icon} alt={app.name} />}

270ec867e329d5eda83321977e32f9811b4c7ef2
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-07-18 15:02:17 +0700

    Fix slug creation for chatbot names with hyphens

--- utils/utils.ts
+++ utils/utils.ts
@@ -48,7 +48,7 @@ export const delay = (ms: number) => {
 };
 
 export const chatbotSlug = (chatbot: any) => {
-  return `${chatbot.name.replaceAll(" ", "-").replaceAll(/[^a-zA-Z0-9-]/g, "")}-${chatbot.chatbot_id}`;
+  return `${chatbot.name.replace(/ /g,'-').replace(/[-]+/g, '-').replace(/[^\w-]+/g,'')}-${chatbot.chatbot_id}`;
 };
 
 export const chatbotIdFromSlug = (slug: string) => {

da9313f1eb57f3fd39b251b25cbafaae998bd4b5
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-08-07 17:22:19 +0700

    Fix research assistant suggestions UI

--- app/(default)/research-assistant/[id]/message-input.tsx
+++ app/(default)/research-assistant/[id]/message-input.tsx
@@ -51,7 +51,7 @@ const ChatInitialSuggestion = ({ handleSendMessage }: any) => {
         (suggestion: string, index: number) => (
           <button
             key={index}
-            className="rounded-lg border border-2 border-[#D1D5DB] bg-container px-4 py-2.5 text-start text-sm font-medium text-heading hover:bg-secondary md:px-8"
+            className="rounded-lg border border-2 border-border bg-container px-4 py-3 text-start text-sm font-medium text-heading hover:bg-secondary md:px-5"
             onClick={(e: any) => handleSendMessage(e, suggestion)}
           >
             {suggestion}
@@ -226,9 +226,9 @@ const MessageInput = () => {
         <ChatInitialSuggestion handleSendMessage={handleSendMessage} />
       )}
 
-      <div className="sticky inset-x-0 bottom-4 mt-2 flex-col items-center gap-2">
+      <div className="sticky inset-x-0 bottom-4 flex-col items-center gap-2">
         {messageHistory.length > 0 && (
-          <div className="mb-4 flex flex-col gap-x-4 gap-y-2 md:grid md:grid-cols-2">
+          <div className="mb-4 flex gap-4 overflow-x-auto max-md:pb-2 md:grid md:grid-cols-3">
             {replyStatus === "idle" &&
               messageHistory[messageHistory.length - 1]?.suggested_questions &&
               messageHistory[
@@ -237,7 +237,7 @@ const MessageInput = () => {
                 (suggestion: string, index: number) => (
                   <button
                     key={index}
-                    className="mt-2 rounded-lg border border-2 border-[#D1D5DB] bg-container px-4 py-2.5 text-start text-sm font-medium text-heading hover:bg-secondary md:px-8"
+                    className="rounded-lg border border-2 border-border bg-container px-4 py-3 text-start text-sm font-medium text-heading hover:bg-secondary max-md:min-w-full md:px-5"
                     onClick={(e: any) => handleSendMessage(e, suggestion)}
                   >
                     {suggestion}

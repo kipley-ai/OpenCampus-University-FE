74a80a795bef86455280b5b44bc317bc320d925e
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-09-30 11:39:23 +0700

    Improve course draft nav

--- app/(educator-platform)/educator/(create-course)/courses/draft/[id]/page.tsx
+++ app/(educator-platform)/educator/(create-course)/courses/draft/[id]/page.tsx
@@ -6,13 +6,63 @@ import { KF_TITLE } from "@/utils/constants";
 import { IntendedLearners } from "./intended-learners";
 import { LandingPage } from "./landing-page";
 
+interface NumberBulletProps {
+  currentStep: number;
+  number: number;
+}
+
+const NumberBullet = ({ currentStep, number }: NumberBulletProps) => {
+  return (
+    <>
+      {currentStep === number ? (
+        <div className="flex size-6 items-center justify-center rounded-full bg-primary text-sidebar">
+          {number}
+        </div>
+      ) : currentStep < number ? (
+        <div className="flex size-6 items-center justify-center rounded-full bg-[#6B7280] text-sidebar">
+          {number}
+        </div>
+      ) : (
+        <div className="flex size-6 items-center justify-center rounded-full bg-[#228403]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="10"
+            fill="none"
+            viewBox="0 0 12 10"
+          >
+            <path
+              fill="#fff"
+              fillRule="evenodd"
+              d="M11.603.675a.656.656 0 0 1 .035.927L4.607 9.195a.656.656 0 0 1-.98-.019L.344 5.332a.656.656 0 0 1 .998-.852l2.802 3.282 6.53-7.051a.656.656 0 0 1 .927-.036Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      )}
+    </>
+  );
+};
+
 export default function CourseDraft() {
-  const [step, setStep] = useState("INTENDED_LEARNERS");
+  const [step, setStep] = useState<string>("INTENDED_LEARNERS");
   const router = useRouter();
+
+  interface StepDict {
+    [key: string]: number;
+  }
+
+  const stepDict: StepDict = {
+    INTENDED_LEARNERS: 1,
+    LANDING_PAGE: 1,
+    CURRICULUM: 2,
+    AI_APPS: 2,
+  };
 
   return (
     <>
       <title>{KF_TITLE + "Course Draft - " + "Learn Java from Scratch"}</title>
+
       <div className="flex h-12 w-full items-center justify-between bg-primary px-8 text-sidebar shadow-lg">
         <div className="flex items-center gap-12">
           <button
@@ -44,36 +94,80 @@ export default function CourseDraft() {
           <span className="text-sm font-bold">Save</span>
         </button>
       </div>
-      <div className="flex items-start gap-16 bg-container px-8 py-8">
-        <div className="mt-8 flex w-fit items-center gap-4">
-          <div className="flex flex-col items-center justify-start gap-4 rounded-full bg-primary-light p-1 text-white">
-            <div className="flex size-6 items-center justify-center rounded-full bg-primary text-sidebar">
-              1
-            </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="6"
-              height="6"
-              fill="none"
-              viewBox="-2 -2 6 6"
-              className="size-6"
-            >
-              <rect width="2" height="2" fill="var(--color-primary)" rx="1" />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="6"
-              height="6"
-              fill="none"
-              viewBox="-2 -2 6 6"
-              className="size-6"
-            >
-              <rect width="2" height="2" fill="var(--color-primary)" rx="1" />
-            </svg>
 
-            <div className="flex size-6 items-center justify-center rounded-full bg-[#6B7280]">
-              2
-            </div>
+      <div className="flex items-start gap-16 bg-container px-8 py-8 max-sm:flex-col">
+        <div className="mt-8 flex w-fit items-start gap-4">
+          <div className="flex flex-col items-center justify-start gap-4 rounded-full bg-primary-light p-1 text-white">
+            <NumberBullet currentStep={stepDict[step]} number={1} />
+            {stepDict[step] === 1 && (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="6"
+                  height="6"
+                  fill="none"
+                  viewBox="-2 -2 6 6"
+                  className="size-6"
+                >
+                  <rect
+                    width="2"
+                    height="2"
+                    fill="var(--color-primary)"
+                    rx="1"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="6"
+                  height="6"
+                  fill="none"
+                  viewBox="-2 -2 6 6"
+                  className="size-6"
+                >
+                  <rect
+                    width="2"
+                    height="2"
+                    fill="var(--color-primary)"
+                    rx="1"
+                  />
+                </svg>
+              </>
+            )}
+            <NumberBullet currentStep={stepDict[step]} number={2} />
+            {stepDict[step] === 2 && (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="6"
+                  height="6"
+                  fill="none"
+                  viewBox="-2 -2 6 6"
+                  className="size-6"
+                >
+                  <rect
+                    width="2"
+                    height="2"
+                    fill="var(--color-primary)"
+                    rx="1"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="6"
+                  height="6"
+                  fill="none"
+                  viewBox="-2 -2 6 6"
+                  className="size-6"
+                >
+                  <rect
+                    width="2"
+                    height="2"
+                    fill="var(--color-primary)"
+                    rx="1"
+                  />
+                </svg>
+              </>
+            )}
             <div className="flex size-6 items-center justify-center rounded-full bg-[#6B7280]">
               3
             </div>
@@ -81,27 +175,58 @@ export default function CourseDraft() {
               4
             </div>
           </div>
-          <div className="flex flex-col items-start justify-start gap-4">
-            <button className="font-semibold text-heading">
-              Plan your course
-            </button>
+
+          <div className="flex flex-col items-start justify-start gap-4 pt-1">
             <button
               onClick={() => setStep("INTENDED_LEARNERS")}
-              disabled={step === "INTENDED_LEARNERS"}
-              className={`ml-4 ${step === "INTENDED_LEARNERS" ? "font-semibold text-primary" : "font-medium text-[#6B7280] hover:text-secondary"}`}
+              disabled={stepDict[step] === 1}
+              className="font-semibold text-heading enabled:hover:text-secondary"
             >
-              Intended learners
+              Plan your course
             </button>
+            {stepDict[step] === 1 && (
+              <>
+                <button
+                  onClick={() => setStep("INTENDED_LEARNERS")}
+                  disabled={step === "INTENDED_LEARNERS"}
+                  className={`ml-4 ${step === "INTENDED_LEARNERS" ? "font-semibold text-primary" : "font-medium text-[#6B7280] hover:text-secondary"}`}
+                >
+                  Intended learners
+                </button>
+                <button
+                  onClick={() => setStep("LANDING_PAGE")}
+                  disabled={step === "LANDING_PAGE"}
+                  className={`ml-4 ${step === "LANDING_PAGE" ? "font-semibold text-primary" : "font-medium text-[#6B7280] hover:text-secondary"}`}
+                >
+                  Landing page
+                </button>
+              </>
+            )}
             <button
-              onClick={() => setStep("LANDING_PAGE")}
-              disabled={step === "LANDING_PAGE"}
-              className={`ml-4 ${step === "LANDING_PAGE" ? "font-semibold text-primary" : "font-medium text-[#6B7280] hover:text-secondary"}`}
+              onClick={() => setStep("CURRICULUM")}
+              disabled={stepDict[step] === 2}
+              className="font-semibold text-heading enabled:hover:text-secondary"
             >
-              Landing page
-            </button>
-            <button className="font-semibold text-heading">
               Create your content
             </button>
+            {stepDict[step] === 2 && (
+              <>
+                <button
+                  onClick={() => setStep("CURRICULUM")}
+                  disabled={step === "CURRICULUM"}
+                  className={`ml-4 ${step === "CURRICULUM" ? "font-semibold text-primary" : "font-medium text-[#6B7280] hover:text-secondary"}`}
+                >
+                  Curriculum
+                </button>
+                <button
+                  onClick={() => setStep("AI_APPS")}
+                  disabled={step === "AI_APPS"}
+                  className={`ml-4 ${step === "AI_APPS" ? "font-semibold text-primary" : "font-medium text-[#6B7280] hover:text-secondary"}`}
+                >
+                  AI apps
+                </button>
+              </>
+            )}
             <button className="font-semibold text-heading">
               Publish your course
             </button>
@@ -110,6 +235,7 @@ export default function CourseDraft() {
             </button>
           </div>
         </div>
+
         {(() => {
           switch (step) {
             case "INTENDED_LEARNERS":

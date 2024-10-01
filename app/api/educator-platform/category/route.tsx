eaa4a7832d38be531c897576e53eb2b55edf5ba1
ridho-lexikat <ridho@lexikat.com>
2024-10-01 10:14:03 +0700

    fix educator api call

--- app/api/educator-platform/category/route.tsx
+++ app/api/educator-platform/category/route.tsx
@@ -1,17 +1,21 @@
 import { NextResponse } from "next/server";
-import { axiosAPI, constructHeader } from "../../utils";
+import { axiosAPI, constructHeader, educatorPlatformHeader } from "../../utils";
+import axios from "axios";
 
 export async function POST(req: Request) {
   try {
     const baseUrl = `${process.env.NEXT_EDUCATOR_BE_URL}/api_v1/category/index`;
-    const res = await axiosAPI(baseUrl, {
+    const res = await axios(baseUrl, {
       method: "POST",
-      headers: await constructHeader(req.headers),
+      headers: educatorPlatformHeader(),
     });
 
     return NextResponse.json(res.data);
   } catch (error) {
     console.error("Error fetching categories:", error);
-    return NextResponse.json({ error: "Failed to fetch categories" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to fetch categories" },
+      { status: 500 },
+    );
   }
 }
--- app/api/utils.tsx
+++ app/api/utils.tsx
@@ -5,43 +5,49 @@ import { getSession } from "next-auth/react";
 // import { getSession } from '@auth0/nextjs-auth0';
 
 export const axiosAPI = axios.create({
-	baseURL: process.env.API_URL,
+  baseURL: process.env.API_URL,
 });
 
 export const constructHeader = async (
-	headers: Headers,
-	ignoreLoginCheck: boolean = false
+  headers: Headers,
+  ignoreLoginCheck: boolean = false,
 ) => {
-	const appId = process.env.APP_ID as string;
-	const apiKey = process.env.API_KEY as string;
-	const nowTimestamp = Math.floor(new Date().getTime() / 1000).toString();
+  const appId = process.env.APP_ID as string;
+  const apiKey = process.env.API_KEY as string;
+  const nowTimestamp = Math.floor(new Date().getTime() / 1000).toString();
 
-	const userId = headers.get("x-kf-user-id"); // wallet address;
+  const userId = headers.get("x-kf-user-id"); // wallet address;
 
-	const commonHeaders = {
-		"x-kf-timestamp": nowTimestamp,
-		"x-kf-sign": createSign(appId + nowTimestamp + apiKey),
-		"x-kf-app-id": appId,
-		"x-kf-request-id": v4(),
-		"x-kf-user-id": userId || "",
-		"x-kf-user-sub": "",
-	};
+  const commonHeaders = {
+    "x-kf-timestamp": nowTimestamp,
+    "x-kf-sign": createSign(appId + nowTimestamp + apiKey),
+    "x-kf-app-id": appId,
+    "x-kf-request-id": v4(),
+    "x-kf-user-id": userId || "",
+    "x-kf-user-sub": "",
+  };
 
-	if (ignoreLoginCheck) {
-		return commonHeaders;
-	}
+  if (ignoreLoginCheck) {
+    return commonHeaders;
+  }
 
-	if (!userId) {
-		const session = await getSession();
+  if (!userId) {
+    const session = await getSession();
 
-		if (session?.user?.username) {
-			commonHeaders["x-kf-user-sub"] = session.user.username;
-		}
-	}
+    if (session?.user?.username) {
+      commonHeaders["x-kf-user-sub"] = session.user.username;
+    }
+  }
 
-	return commonHeaders;
+  return commonHeaders;
 };
 
 const createSign = (content: string) => {
-	return sha256(content).toString().toUpperCase();
+  return sha256(content).toString().toUpperCase();
+};
+
+export const educatorPlatformHeader = () => {
+  return {
+    "x-api-key": process.env.NEXT_PUBLIC_EDUCATOR_BE_API_KEY!,
+  };
 };
